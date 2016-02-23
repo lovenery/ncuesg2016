3cd7cd1d21eb3a134f925a4406229062f27b78ef
lovenery <edittye379@gmail.com>
2016-02-23 23:16:29 +0800

    bug?

--- src/routes/team.js
+++ src/routes/team.js
@@ -177,7 +177,8 @@ router.get('/new', isLoggedIn, function(req, res) {
 
 
 var uploadHead = multer({
-  dest: './src/public/uploads/',
+  dest: './public/uploads',
+  //dest: './src/public/uploads',
   //dest: './public/uploads',
   onFileUploadStart: function (file, req, res) {
     console.log(file.fieldname + ' is starting ...');
